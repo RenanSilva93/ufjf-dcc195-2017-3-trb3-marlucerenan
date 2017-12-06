20c625b6440c297090570c2e9fda8df6ff18aade
home <marlucecancio@gmail.com>
2017-12-06 21:01:04 -0200

    d

--- app/routes/compras.routes.js
+++ app/routes/compras.routes.js
@@ -1,7 +1,7 @@
 module.exports = function (app){
   var rota = require("../controllers/compras.controllers")
-  app.use("/compras.html",rota.compras)
-  app.use("/listar_compras.html",rota.listarCompras)
-  app.use("/listar_compras.html",rota.listarCompras)
-  app.use("/nova_compras.html",rota.novaCompras)
+  app.use("/compras/compras.html",rota.compras)
+  app.use("/compras/listar_compras.html",rota.listarCompras)
+//  app.use("/listar_compras.html",rota.listarCompras)
+  app.use("/compras/nova_compras.html",rota.novaCompras)
 }
