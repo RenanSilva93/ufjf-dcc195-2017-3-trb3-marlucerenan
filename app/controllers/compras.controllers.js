c97a6f5b9638bfaa11ca897013e256b53dd93046
RenanSilva93 <renan.costas@hotmail.com>
2017-12-06 20:43:31 -0200

    Adicionado arquivos de novas compras

--- app/controllers/compras.controllers.js
+++ app/controllers/compras.controllers.js
@@ -21,3 +21,68 @@ module.exports.listarCompras = function(req,res,next){
      return next(err);
    });
 }
+
+module.exports.novaCompras = function(req, res, next) {
+  if(req.method=='GET'){
+   res.render('compras/novo',{'usuarioLogado':req.session.usuarioLogado});
+ }else {
+   var novo = new Compras(req.body);
+   novo.save().then(
+   function(Compras){
+       calculaPrecos()
+       res.redirect("/compras/listar_compras.html");
+   },
+   function(err){
+     return next(err);
+   });
+ }
+}
+
+function calculaPrecos(){
+  Compras.find({}).then(
+   function(Compras){
+     var maiorInteresse = Compras[0];
+     for(var i in Compras){
+       var compras = Compras[i]
+       if(compras.somaInteresse>maiorInteresse.somaInteresse){
+         maiorInteresse = compras;
+       }
+     }
+     if(maiorInteresse.soma_interesse > 0){
+       for(var i in Compras){
+         var compras = Compras[i]
+         if(compras.somaInteresse > 0){
+           compras.preco = compras.preco_base + compras.preco_base*(compras.somaInteresse/maiorInteresse.somaInteresse);
+           salvarCompras(compras)
+         }else if(compras.somaInteresse == 0&& compras.preco == 0){
+               compras.preco = compras.preco_base;
+               salvarCompras(compras)
+         }
+       }
+     }else{
+       for(var i in Compras){
+         var compras = Compras[i]
+         if(compras.preco == 0){
+           compras.preco = compras.preco_base
+           salvarCompras(compras)
+         }
+       }
+     }
+   },
+   function(err){
+     return next(err);
+   });
+}
+
+function salvarCompras(compras){
+  Compras.findByIdAndUpdate(
+    compras._id,
+    compras,
+    {new: true}
+  ).then(
+    function (compras){},
+    function(err) {
+      return next(err);
+    }
+  );
+}
--- app/routes/compras.routes.js
+++ app/routes/compras.routes.js
@@ -2,4 +2,6 @@ module.exports = function (app){
   var rota = require("../controllers/compras.controllers")
   app.use("/compras.html",rota.compras)
   app.use("/listar_compras.html",rota.listarCompras)
+  app.use("/listar_compras.html",rota.listarCompras)
+  app.use("/nova_compras.html",rota.novaCompras)
 }
