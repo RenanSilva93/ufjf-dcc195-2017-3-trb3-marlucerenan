21f6367b49afc8e51fead20c516ea0a2a777c571
home <marlucecancio@gmail.com>
2017-12-06 19:38:20 -0200

    atualização de arquivos sobre

---
+++ app/controllers/sobre.controllers.js
@@ -0,0 +1,10 @@
+module.exports.sobre = function (req, res, next){
+  res.render('sobre/sobre',
+  {nome1: "Renan Costa da Silva",
+	curso: "Sistemas de Informação",
+	matricula1: "201276048",
+	email1: "renan.costas@hotmail.com",
+	nome2: "Marluce Aparecida Vitor",
+	matricula2: "201276026",
+	email2: "marlucecancio@gmail.com"})
+}
